018a77d4d0c4acb042a2433d1069d18a1a9924d1
minggul <forrest_gump_@naver.com>
2021-05-14 00:47:44 +0900

    json 응답 예제 추가

---
+++ src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
@@ -0,0 +1,33 @@
+package hello.servlet.basic.response;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //Content-Type: application/json
+        resp.setContentType("application/json");    // 스펙상 utf-8이 기본이기때문에 application/json;charset=utf-8 이렇게 적게되면 불필요한 코드임
+        resp.setCharacterEncoding("utf-8");
+
+        HelloData helloData = new HelloData();
+        helloData.setUsername("kim");
+        helloData.setAge(20);
+
+        //{"username":"kim", "age":20}
+        String result = objectMapper.writeValueAsString(helloData);
+        resp.getWriter().write(result);
+    }
+}
